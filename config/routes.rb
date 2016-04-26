4cbd0c29b57edc85688656046b440ba9f38f7b19
fabien <fabien.lewandowski@gmail.com>
2016-04-26 11:15:14 +0200

    Fin du layout et des routages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/contact'
+  get 'users/new'
 
-  get 'pages/about'
+  get '/signup',  :to => 'users#new'
+  get '/contact', :to => 'pages#contact'
+  get '/about',   :to => 'pages#about'
+  get '/help',    :to => 'pages#help'
 
-  get 'pages/help'
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
